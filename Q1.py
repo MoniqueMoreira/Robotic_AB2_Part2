954685c65d6425729e45bb4ecbd4e634766b62a6
MoniqueMoreira <mmm@ic.ufal.br>
2023-10-07 16:42:20 -0300

    Questão 1

--- Q1.py
+++ Q1.py
@@ -6,10 +6,14 @@ from spatialmath import SE3
 from spatialmath.base import *
 from roboticstoolbox import jsingu,xplot ,ET2, DHRobot, RevoluteDH, PrismaticDH
 
+
 def plots(qs,erro,t):
 
-    plt.plot(erro,t, label = "Erro X Tempo")
+    plt.plot(erro,t)
+    plt.xlabel('Tempo')
+    plt.ylabel('Erro')
     plt.legend()
+    plt.title("Erro X Tempo")
     plt.show()
     # Extraia as posições de cada junta em listas separadas
     theta1_positions = [pos[0] for pos in qs]
@@ -31,7 +35,8 @@ def plots(qs,erro,t):
     plt.legend()
 
     # Exiba os gráficos
-    #plt.tight_layout()
+    plt.tight_layout()
+    plt.title("Junta x Tempo")
     plt.show()
 
 def rr_robot(L1=1, L2=1):
@@ -42,28 +47,37 @@ def rr_robot(L1=1, L2=1):
     rob = DHRobot([e1,e2], name = 'RR')
     return rob
 
-def resolved_rate_control_2r(L1 = 1,L2=1):
-    """
-    Resolved rate control for the RR robot.
-    """
+def trajetoria():
     rr = rr_robot()
 
     q0 = np.array([-np.pi / 3, np.pi / 2])
     #print(q0)
 
     TE1 = rr.fkine(q0)
-    #print(TE1)
-    TE2 = SE3.Trans(0.5,0.5, 0) @ TE1
-    #print(TE2)
+    print("Pose inicial:\n" ,TE1)
+    TE2 = SE3.Trans(0,-0.5, 0) @ TE1
+    print("Pose final:\n", TE2)
 
     t = np.arange(0, 5, 0.02)
     #print(t)
 
     Ts = ctraj(TE1, TE2, t)
     #print(Ts)
-    #xplot(t, Ts.t, labels="x y z")
-    #xplot(t, Ts.rpy("xyz"), labels="roll pitch yaw")
-    #plt.show()
+    xplot(t, Ts.t, labels="x y z")
+    xplot(t, Ts.rpy("xyz"), labels="roll pitch yaw")
+    plt.show()
+    return Ts
+
+
+def resolved_rate_control_2r(L1 = 1,L2=1):
+    """
+    Resolved rate control for the RR robot.
+    """
+    rr = rr_robot()
+   
+    Ts = trajetoria()
+
+    t = np.arange(0, 5, 0.02)
 
     # Taxa de atualização de controle
     dt = 0.01
@@ -91,15 +105,15 @@ def resolved_rate_control_2r(L1 = 1,L2=1):
         error_y = y - (L1 * np.sin(theta1) + L2 * np.sin(theta1 + theta2))
 
         # Calcule as velocidades das juntas usando a matriz Jacobiana
-        J = np.array([[-(L1 + L2*np.cos(theta2))*np.sin(theta1),-L2*np.cos(theta1)*np.sin(theta2)],
-                  [np.cos(theta1)*(L1 + L2*np.sin(theta2)),L2*np.cos(theta2)*np.sin(theta1)]])
+        J = np.array([[-L1*np.sin(theta1) - L2*np.sin(theta1 + theta2),-L2*np.sin(theta1 + theta2)],
+                      [L1*np.cos(theta1) + L2*np.cos(theta1 + theta2),L2*np.cos(theta1 + theta2)]])
         #print(J)
         #print(jsingu(J))
 
         # Resolva a equação de controle: J * [dtheta1, dtheta2] = [error_x, error_y]
         determinant = np.linalg.det(J)
         if abs(determinant) < 1e-6:
-            lambda_value = 0.02  # Ajuste este valor conforme necessário
+            lambda_value = 0.01 # Ajuste este valor conforme necessário
             J_reg = J + lambda_value * np.identity(J.shape[0])
             joint_velocities = np.linalg.solve(J_reg, np.array([error_x, error_y]))
 
@@ -113,6 +127,7 @@ def resolved_rate_control_2r(L1 = 1,L2=1):
 
         q = [theta1,theta2]
         qs.append(q)
+        #rr.plot(q)
 
         pos = rr.fkine(q).t[:2]
         k = np.linalg.norm(pos - Ts[-1].t[:2])
@@ -122,6 +137,12 @@ def resolved_rate_control_2r(L1 = 1,L2=1):
         # Aguarde um tempo para a próxima iteração
         time.sleep(dt)
 
-    plots(qs,erro,t)
-    #rr.plot(q = qs)
+    print("q = {}".format([theta1,theta2]))
+    #print(np.rad2deg(theta1), np.rad2deg(theta2))
 
+    rr.teach([theta1,theta2])
+    plots(qs,erro,t)
+    
+
+def main():
+    resolved_rate_control_2r()
